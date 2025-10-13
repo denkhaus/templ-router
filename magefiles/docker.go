7be0c7eeedf27ea8c30695618c365e96f5fa728d
denkhaus <peristaltic@gmx.net>
2025-10-13 13:31:43 +0200

    fix: Update template-generator installation to use 'main' branch and refactor Docker command methods for consistency

--- magefiles/docker.go
+++ magefiles/docker.go
@@ -11,30 +11,30 @@ import (
 type Docker mg.Namespace
 
 // DevUp starts development Docker services
-func (Docker) Up() error {
+func (p Docker) Up() error {
 	fmt.Println("Starting development Docker services...")
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "up", "-d")
 }
 
 // DevDown stops development Docker services
-func (Docker) Down() error {
+func (p Docker) Down() error {
 	fmt.Println("Stopping development Docker services...")
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "down")
 }
 
-func (Docker) Clean() error {
+func (p Docker) Clean() error {
 	fmt.Println("Cleaning development Docker services...")
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "down", "-v", "--remove-orphans")
 }
 
 // Logs shows Docker logs
-func (Docker) Logs() error {
+func (p Docker) Logs() error {
 	fmt.Println("Showing Docker logs...")
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "logs", "-f")
 }
 
 // Build builds the Docker image
-func (Docker) Build() error {
+func (p Docker) Build() error {
 	fmt.Println("Building Docker image...")
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "build")
 }
@@ -45,7 +45,7 @@ func (p Docker) Rebuild() error {
 	return sh.RunV("docker-compose", "-f", "docker/docker-compose.dev.yml", "build", "--no-cache")
 }
 
-func (p Docker) Restart() error {
+func (p Docker) StartClean() error {
 	mg.SerialDeps(p.Clean, p.Rebuild, p.Up)
 	fmt.Println("Restarting successfull...")
 	return nil
